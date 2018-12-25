e02b38aa7b69786e89c082764cef39f289d957da
AS <anis.kh86@gmail.com>
2018-12-25 21:14:24 +0100

    add display books component with graphql query

--- src/component/BooksComponent/DisplayBooks.js
+++ src/component/BooksComponent/DisplayBooks.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { graphql, compose } from "react-apollo";
+import queries from "../../queries/bookQueries";
 
 class DisplayBooks extends React.Component {
+  // function that get back the result of GetBooksQuery and display it
+  displayBooks = () => {
+    let { Books, loading } = this.props.getBooksQuery;
+    if (loading) {
+      return <div> loading</div>;
+    } else {
+      return Books.map((e, i) => {
+        return (
+          <ul key={i}>
+            <li>{e.name}</li>
+            <li>{e.language}</li>
+            <li>{e.summary}</li>
+          </ul>
+        );
+      });
+    }
+  };
+
   render() {
-    return <div>Display Books Component</div>;
+    return (
+      <div>
+        <h1>Display Books Component</h1>
+        {this.displayBooks()}
+      </div>
+    );
   }
 }
 
-export default DisplayBooks;
+export default compose(graphql(queries.getBooks, { name: "getBooksQuery" }))(
+  DisplayBooks
+);
